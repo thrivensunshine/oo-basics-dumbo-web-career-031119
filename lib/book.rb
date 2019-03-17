c17ece0c675a1b6617c7f73b71107c0c79f29167
DANIELLE ALBA <danib.sgh@gmail.com>
2019-03-17 15:43:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
-
+class Book
+  
+  def initialize(title)
+    @title = title 
+    @author_name = author_name
+    @page_count = page_count
+    @genre = genre
+  end 
+  
+  def turn_page
+  
+end
+  
+end
 
