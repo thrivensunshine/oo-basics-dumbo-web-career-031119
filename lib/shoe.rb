19dccce4cdab2acee838f860ea65dfa7c164fc1c
DANIELLE ALBA <danib.sgh@gmail.com>
2019-03-17 15:50:43 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor
+  attr_accessor :brand, :color, :size, :material, :condition
   def initialize(brand)
     @brand = brand
     @color = color
