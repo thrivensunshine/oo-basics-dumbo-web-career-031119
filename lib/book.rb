e847f517dcaa1cb68834b8789c030c35cee25843
DANIELLE ALBA <danib.sgh@gmail.com>
2019-03-17 15:45:56 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :title, :author, :page_count, :genre
   def initialize(title)
     @title = title 
-    @author = author_name
+    @author = author
     @page_count = page_count
     @genre = genre
   end 
